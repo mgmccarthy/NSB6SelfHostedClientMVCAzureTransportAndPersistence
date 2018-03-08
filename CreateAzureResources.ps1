fcc42ca9f0e5c07c3f527f0a0aa362036c05771a
Michael McCarthy <mike_mccarthy3@yahoo.com>
2018-03-08 10:02:31 -0500

    Update CreateAzureResource to take params for $resourceGroupName, $storageAccountName and $location when invoking it from the command line for

--- CreateAzureResources.ps1
+++ CreateAzureResources.ps1
@@ -1,8 +1,9 @@
-$resourceGroupName = ""
-$storageAccountName = ""
-$sku = ""
-$location = ""
+param(
+	[string]$resourceGroupName, 
+	[string]$storageAccountName,
+	[string]$location
+)
 
 az group create --name $resourceGroupName --location $location
-az storage account create --location eastus --name $storageAccountName --resource-group $resourceGroupName --sku $sku
+az storage account create --location eastus --name $storageAccountName --resource-group $resourceGroupName --sku "Standard_LRS"
 az storage account show-connection-string --name $storageAccountName --resource-group $resourceGroupName
